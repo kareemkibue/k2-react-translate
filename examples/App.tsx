577eede72115f18e422f421bd2a8a15570563d40
Kareem Kibue <kareemkibue@gmail.com>
2019-11-20 14:09:26 -0500

    🔧 Config: Setup examples webpack

---
+++ examples/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+
+const App: React.FunctionComponent<{}> = () => {
+	return <div >Test</div>;
+};
+
+export { App };
---
+++ examples/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { App } from './App';
+
+ReactDOM.render(
+	// <Provider {...{ store }}>
+	<App />,
+	// </Provider>
+	document.getElementById('root')
+);
