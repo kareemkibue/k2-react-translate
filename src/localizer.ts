99964fc45e3e46e569cc06c842ca2d6ff44741ed
Kareem Kibue <kareemkibue@gmail.com>
2019-11-27 20:49:48 -0500

    Update: Externalize getLanguage from url

--- src/localizer.ts
+++ src/localizer.ts
@@ -1,52 +1,60 @@
 const getLocalizedUrl = (language: string, url: string): string => {
-  return `/${language + url}`;
+    return `/${language + url}`;
 };
 
 // TODO Resolve types, eliminate any - @kareemkibue
 const getLocalizedRoute = (language: string | null, url: any): any => {
-  const languagePrefix: string = language === null ? ':language' : language;
+    const languagePrefix: string = language === null ? ':language' : language;
 
-  if (typeof url === 'undefined') {
-    return url;
-  }
-  // TODO Fix to allow for external linking - @kareemkibue
-  // if (url.indexOf('http') > -1 || url.indexOf('//') > -1) {
-  //     console.log(url)
-  //     return url;
-  // }
-  if (typeof url === 'string') {
-    return getLocalizedUrl(languagePrefix, url);
-  }
+    if (typeof url === 'undefined') {
+        return url;
+    }
+    // TODO Fix to allow for external linking - @kareemkibue
+    // if (url.indexOf('http') > -1 || url.indexOf('//') > -1) {
+    //     console.log(url)
+    //     return url;
+    // }
+    if (typeof url === 'string') {
+        return getLocalizedUrl(languagePrefix, url);
+    }
 
-  // * if the history push arg is an object, ie { pathname: sting, search: query, state: someState}
-  return {
-    ...url,
-    pathname: getLocalizedUrl(languagePrefix, url.pathname),
-  };
+    // * if the history push arg is an object, ie { pathname: sting, search: query, state: someState}
+    return {
+        ...url,
+        pathname: getLocalizedUrl(languagePrefix, url.pathname),
+    };
 };
 
 const concatenateLanguageParam = (path: string): string => {
-  return '/:language' + path;
+    return '/:language' + path;
 };
 
 const getLocalizedPath = (
-  path: string | string[]
+    path: string | string[]
 ): string | string[] | undefined => {
-  if (Array.isArray(path)) {
-    return path.map((singlePath: string) =>
-      concatenateLanguageParam(singlePath)
-    );
-  }
-  if (typeof path === 'string') {
-    return concatenateLanguageParam(path);
-  }
+    if (Array.isArray(path)) {
+        return path.map((singlePath: string) =>
+            concatenateLanguageParam(singlePath)
+        );
+    }
+    if (typeof path === 'string') {
+        return concatenateLanguageParam(path);
+    }
 
-  return undefined;
+    return undefined;
 };
 
+const getLanguageFromUrl = (languages: string[], url: string = window.location.pathname): string | null => {
+    return languages.find((language: string) => {
+
+        return url.toLowerCase().includes(language.toLowerCase());
+    }) || ''
+}
+
 const localizer = {
-  getLocalizedRoute,
-  getLocalizedPath,
+    getLocalizedRoute,
+    getLocalizedPath,
+    getLanguageFromUrl
 };
 
 export { localizer };
